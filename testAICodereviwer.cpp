40ca9456bb8b62ed8a39c577b345eceb88caecaf
Kandunuri Kalyani <168817647+KalyaniKandunuriTR@users.noreply.github.com>
2025-03-05 18:07:20 +0530

    Create testAICodereviwer.cpp

---
+++ testAICodereviwer.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+ 
+class Example {
+
+public:
+
+    int* data;
+ 
+    Example() {
+
+        data = new int[10];
+
+        for (int i = 0; i < 10; ++i) {
+
+            data[i] = i;  // Initialize the array to prevent undefined behavior
+
+        }
+
+    }
+
+    ~Example() {
+
+        delete[] data;
+
+    }
+
+    void processData() {
+
+        std::cout << "Data: " << data[0] << std::endl;
+
+    }
+
+    void correctLogic(int a, int b) {
+
+        if (a < b) {
+
+            std::cout << "a is less than b" << std::endl;
+
+        } else {
+
+            std::cout << "a is greater than or equal to b" << std::endl;
+
+        }
+
+    }
+
+};
+ 
+int main() {
+
+    Example* example = new Example();
+
+    example->processData();
+
+    example->correctLogic(5, 3);
+
+    delete example;
+
+    return 0;
+
+}
+ 
